313e71de22e9dea4f57da4c77c84461048bb4dbc
Rav2k <102990625+Rav2k@users.noreply.github.com>
2022-11-21 14:18:34 -0800

    Update media.cpp

--- media.cpp
+++ media.cpp
@@ -7,13 +7,13 @@ media::media(){
   title = new char[20];
 }
 
-char* media::gettitle(){
+char* media::gettitle(){// when asking for the title of a media in the class cpp file it will return the title, which is a variable in the media.h file that is different for each child class(music, movies videogames)
   return title;
 }
 
-int media::getyear(){
+int media::getyear(){//same as title but for year
   return year;
 }
 
-void media::printStuff(){  
+void media::printStuff(){
 }
