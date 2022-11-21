d86a9d5b7c5d60f6a61f30f6a78a43c24cf13712
Rav2k <102990625+Rav2k@users.noreply.github.com>
2022-11-21 14:23:37 -0800

    Update media.h

--- media.h
+++ media.h
@@ -6,12 +6,12 @@ using namespace std;
 
 class media{
  public:
-  virtual void printStuff();
+  virtual void printStuff(); // virtual because each media in the storage is different so when you're trying to find something and you know the media type you look in that medis type and print it's print stuff function.
   media();
-  char* gettitle();
-  int getyear();
-  int year;
-  char* title;
+  char* gettitle();//function for getting title.
+  int getyear();//function for getting year
+  int year;//varies by child
+  char* title;//varies by child
 };
 
 #endif
