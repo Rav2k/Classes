62159a36699ed87daec759ca6ab319a51864ba72
Rav2k <102990625+Rav2k@users.noreply.github.com>
2022-11-21 15:43:10 -0800

    Update movies.cpp

--- movies.cpp
+++ movies.cpp
@@ -8,19 +8,19 @@ movies::movies(){
   director = new char[20];
 }
 
-char* movies::getDirector(){
+char* movies::getDirector(){//function to return the direction of the movies.
   return director;
 }
 
-double movies::getRating(){
+double movies::getRating(){//function to return the rating of the movie.
   return rating;
 }
 
-double movies::getDuration(){
+double movies::getDuration(){//function to return the durantion of the movie.
   return duration;
 }
 
-void movies::printStuff(){
+void movies::printStuff(){//function to print the stuff in movie storage
     cout<<title<<endl;
     cout<<director<<endl;
     cout<<year<<endl;
