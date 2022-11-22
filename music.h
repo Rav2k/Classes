b0cc8a93dd3b680f31d3838e259d54e1c9d17948
Rav2k <102990625+Rav2k@users.noreply.github.com>
2022-11-21 21:28:06 -0800

    Update music.h

--- music.h
+++ music.h
@@ -8,10 +8,10 @@ class music: public media{
  public:
   music();
   void printStuff();
-  char* getpublisher();
-  char* getartist();
-  double getduration();
-  double duration;
-  char *publisher;
-  char *artist;
+  char* getpublisher();// function for getting the publisher for media
+  char* getartist();//function for getting the artist fo media 
+  double getduration();//function for getting the duration of the media
+  double duration;//variable for getting the duration
+  char *publisher;//variable for getting the publisher
+  char *artist;//variable for getting the artist 
 };
